08a9bf343c18a8412e47cbbf7a268d5ab7f0cb58
GuyThatHelps <33295280+RKasmier@users.noreply.github.com>
2019-05-07 12:13:02 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,9 +7,24 @@ window.onload=function(){
   
 }
 
+x=canv.width/2;
+y=canv.height/2;
+xv=yv=0;
+
+xv = 5;
 function main(){
   ctx.fillStyle = "black";
    ctx.fillRect(0,0, canv.width, canv.height);
+  ctx.fillStyle = "white";
   
-  
+  ctx.fillEllipse(x, y, 25, 25);
+   
+  if(x>canv.width-25 || x<25){
+     xv *= -1;
+     }
+   if(y>canv.height || y<25){
+     yv *= -1;
+   }
+  x+=xv;
+  y+=yv;
 }
