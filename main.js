fcac5b35e45b77d8a35b7d5936ae865614687530
GuyThatHelps <33295280+RKasmier@users.noreply.github.com>
2019-05-07 08:41:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+window.onload=function(){
+  canv = document.getElementById("canvas");
+  ctx = canv.getContext("2d");
+  
+  setInterval(main, 1000/120);
+  
+}
+
+function main(){
+   alert("works");
+  
+}
